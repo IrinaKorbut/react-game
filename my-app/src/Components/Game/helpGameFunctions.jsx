fe4c98953806d00d2f628e853143cc3b8920918e
IrinaKorbut <irisharomka96@gmail.com>
2021-02-20 20:40:10 +0300

    fix: render game field

--- my-app/src/Components/Game/helpGameFunctions.jsx
+++ my-app/src/Components/Game/helpGameFunctions.jsx
@@ -3,13 +3,18 @@ import { GameCell } from './GameCell';
 export function renderGameField(size) {
     const gameMatrix = []
     for (let i = 0; i < size; i++) {
-        const arr = Array(size).fill(0)
-        const arrNumbers = arr.map(function (item) {
-            return item + Math.floor(Math.random() * 9) + 1
+        const arr = Array(size).fill({})
+        const arrNumbers = arr.map(function (item, index) {
+            return {
+                i: i,
+                j: index,
+                value: Math.floor(Math.random() * 9) + 1
+            }
+
         });
         gameMatrix.push(arrNumbers);
     }
-    return gameMatrix;
+    return gameMatrix
 }
 
 export function clickHandler() {
